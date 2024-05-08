ba6d193aea2eff49f4b4f66cd6e1dc817ee6466e
nissy-dev <nd.12021218@gmail.com>
2024-05-08 23:26:55 +0900

    wip

---
+++ tcp/toytcp/examples/echoclient.rs
@@ -0,0 +1,17 @@
+use anyhow::Result;
+use std::net::Ipv4Addr;
+use toytcp::TcpClient;
+
+fn main() -> Result<()> {
+    let args = std::env::args().collect::<Vec<String>>();
+    let addr: Ipv4Addr = args[1].parse()?;
+    let port: u16 = args[2].parse()?;
+    echo_client(addr, port)?;
+    Ok(())
+}
+
+fn echo_client(remote_addr: Ipv4Addr, remote_port: u16) -> Result<()> {
+    let mut client = TcpClient::new();
+    let _ = client.connect(remote_addr, remote_port)?;
+    Ok(())
+}
