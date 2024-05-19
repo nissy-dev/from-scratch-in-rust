7cd5958cfec895b254a5dd7db9ec5fb597f50b05
nissy-dev <nd.12021218@gmail.com>
2024-05-19 12:18:08 +0900

    wip

--- tcp/toytcp/src/packet.rs
+++ tcp/toytcp/src/packet.rs
@@ -1,8 +1,14 @@
-use pnet::packet::Packet;
+use core::fmt;
+use std::fmt::Debug;
+use std::net::Ipv4Addr;
+
+use pnet::packet::{ip::IpNextHeaderProtocols, tcp::TcpPacket, util, Packet};
+
+use crate::tcpflags;
 
 const TCP_HEADER_SIZE: usize = 20;
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub struct TCPPacket {
     buffer: Vec<u8>,
 }
@@ -21,8 +27,83 @@ impl TCPPacket {
         self.buffer[2..4].copy_from_slice(&port.to_be_bytes());
     }
 
+    pub fn set_seq(&mut self, num: u32) {
+        self.buffer[4..8].copy_from_slice(&num.to_be_bytes());
+    }
+
+    pub fn set_ack(&mut self, num: u32) {
+        self.buffer[8..12].copy_from_slice(&num.to_be_bytes());
+    }
+
+    pub fn set_data_offset(&mut self, offset: u8) {
+        self.buffer[12] |= offset << 4;
+    }
+
     pub fn set_flag(&mut self, flag: u8) {
         self.buffer[13] = flag;
+    }
+
+    pub fn set_window_size(&mut self, window: u16) {
+        self.buffer[14..16].copy_from_slice(&window.to_be_bytes());
+    }
+
+    pub fn set_checksum(&mut self, checksum: u16) {
+        self.buffer[16..18].copy_from_slice(&checksum.to_be_bytes());
+    }
+
+    pub fn set_payload(&mut self, payload: &[u8]) {
+        self.buffer[TCP_HEADER_SIZE..TCP_HEADER_SIZE + payload.len() as usize]
+            .copy_from_slice(payload)
+    }
+
+    pub fn is_correct_checksum(&self, local_addr: Ipv4Addr, remote_addr: Ipv4Addr) -> bool {
+        self.get_checksum()
+            == util::ipv4_checksum(
+                &self.packet(),
+                8,
+                &[],
+                &local_addr,
+                &remote_addr,
+                IpNextHeaderProtocols::Tcp,
+            )
+    }
+
+    pub fn get_src(&self) -> u16 {
+        u16::from_be_bytes([self.buffer[0], self.buffer[1]])
+    }
+
+    pub fn get_dest(&self) -> u16 {
+        u16::from_be_bytes([self.buffer[2], self.buffer[3]])
+    }
+
+    pub fn get_seq(&self) -> u32 {
+        u32::from_be_bytes([
+            self.buffer[4],
+            self.buffer[5],
+            self.buffer[6],
+            self.buffer[7],
+        ])
+    }
+
+    pub fn get_ack(&self) -> u32 {
+        u32::from_be_bytes([
+            self.buffer[8],
+            self.buffer[9],
+            self.buffer[10],
+            self.buffer[11],
+        ])
+    }
+
+    pub fn get_flag(&self) -> u8 {
+        self.buffer[13]
+    }
+
+    pub fn get_window_size(&self) -> u16 {
+        u16::from_be_bytes([self.buffer[14], self.buffer[15]])
+    }
+
+    pub fn get_checksum(&self) -> u16 {
+        u16::from_be_bytes([self.buffer[16], self.buffer[17]])
     }
 }
 
@@ -33,5 +114,30 @@ impl Packet for TCPPacket {
 
     fn payload(&self) -> &[u8] {
         &self.buffer[TCP_HEADER_SIZE..]
+    }
+}
+
+impl Debug for TCPPacket {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            r"
+        src: {}
+        dst: {}
+        flag: {}
+        payload: {}",
+            self.get_src(),
+            self.get_dest(),
+            tcpflags::flag_to_string(self.get_flag()),
+            self.payload().len()
+        )
+    }
+}
+
+impl<'a> From<TcpPacket<'a>> for TCPPacket {
+    fn from(packet: TcpPacket) -> Self {
+        Self {
+            buffer: packet.packet().to_vec(),
+        }
     }
 }
